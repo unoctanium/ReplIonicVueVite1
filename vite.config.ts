bedcc1a53222410e2616e8c80efa0b2d0cbecefb
Odo Maletzki <odo@maletzki.net>
2022-02-20 06:16:36 +0000

    Initial

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+
+export default {
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      port: 443,
+    }
+  }
+}
